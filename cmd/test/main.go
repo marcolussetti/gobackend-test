325d30053d5006c24587d5f66d9eb2b6d14bf9b4
Marco Lussetti <marcolussetti@users.noreply.github.com>
2020-06-09 22:41:09 -0700

    Continued work, but scrapping as tutorial is broken

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -53,11 +53,56 @@ func getAllEvents(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(events)
 }
 
+func updateEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+	var updatedEvent Event
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	fmt.Printf("Whatever " + string(reqBody))
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+	json.Unmarshal(reqBody, &updatedEvent)
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			singleEvent.Title = updatedEvent.Title
+			singleEvent.Description = updatedEvent.Description
+			events[i] = singleEvent
+			json.NewEncoder(w).Encode(events[i])
+		}
+	}
+
+}
+
+// func updateEvent(w http.ResponseWriter, r *http.Request) {
+// 	// Get the ID from the url
+// 	eventID := mux.Vars(r)["id"]
+// 	var updatedEvent Event
+// 	// Convert r.Body into a readable formart
+// 	reqBody, err := ioutil.ReadAll(r.Body)
+// 	if err != nil {
+// 		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+// 	}
+
+// 	json.Unmarshal(reqBody, &updatedEvent)
+
+// 	for i, singleEvent := range events {
+// 		if singleEvent.ID == eventID {
+// 			singleEvent.Title = updatedEvent.Title
+// 			singleEvent.Description = updatedEvent.Description
+// 			events[i] = singleEvent
+// 			json.NewEncoder(w).Encode(singleEvent)
+// 		}
+// 	}
+// }
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeHandler)
 	router.HandleFunc("/event", createEvent).Methods("POST")
 	router.HandleFunc("/events", getAllEvents).Methods("GET")
 	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
+	router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
